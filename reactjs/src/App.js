0bea61bb018dbe0f83df8d8c1f5f1ef181add69d
EduFerDias <Diasdu2011@outlook.com>
2021-09-24 16:53:04 -0300

    ok

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactsx
         </a>
       </header>
     </div>
