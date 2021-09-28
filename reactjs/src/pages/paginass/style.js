8e0c3313018647b5710995d9e3eaa75161665864
matheus <rafaelmatheus061@gmail.com>
2021-09-28 19:30:30 -0300

    oi

--- reactjs/src/pages/paginass/style.js
+++ reactjs/src/pages/paginass/style.js
@@ -2,27 +2,70 @@ import styled from "styled-components";
 
 const Conteudo= styled.div`
 
+@font-face {
+    font-family: Montserrat;
+    src: url(./assets/fonts/Montserrat-Bold.ttf);
+  }
+  
+  @font-face {
+    font-family: Regular;
+    src: url(./assets/fonts/Montserrat-Regular.ttf);
+}
+
+
+body {
+    margin: 0px;
+}
+
 * {
-    background-color: #404756;
+    box-sizing: border-box;
+    cursor: default;
 }
-
+  
 div {
-    justify-content: center;
-    flex-direction: column;
+    justify-content:
     display: flex;
-    color: white;
-    padding: 19vw;
-}
+    flex-direction: column;
 
-a {
-    color: white;
-    font-size: 20px;
-    line-height: 2;
+    background-image: url("./assets/images/Cadastro e Login/p1-estaiada.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    width: 100vw;
+    height: 100vh;
+    margin: 0 auto;
+    background-color: #fff;
+
+    align-items: center;
+}
+  
+button {
+    width: 300px;
+    height: 25px;
+
+    font-family: Montserrat;
+    font-style: normal;
     font-weight: bold;
-    text-align: center;
-    font-family: monospace;
-}
+    font-size: 15px;
 
+    text-align: center;
+
+    color: #ffffff;
+
+    background: #107ad1;
+    border-radius: 12px;
+
+    cursor: pointer;
+
+    border: none;
+
+    margin-top: 0.5em;
+}
+  
+button:hover {
+    background-color: rgba(0, 85, 255, 1);
+}
+  
 `
 
 export default Conteudo
