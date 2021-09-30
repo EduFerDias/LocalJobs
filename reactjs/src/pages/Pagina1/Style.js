4b7e6f6cc07a5fc138bdbe734fd60254d0893c78
MatheusRafaDev <90002960+MatheusRafaDev@users.noreply.github.com>
2021-09-30 17:37:41 -0300

    Update Style.js

--- reactjs/src/pages/Pagina1/Style.js
+++ reactjs/src/pages/Pagina1/Style.js
@@ -9,8 +9,7 @@ button Link{
 .faixa1{
     background-image: url("./assets/images/Cadastro e Login/p1-carteira-de-trabalho-primeiro-emprego-contratar-contratacao-1608066576426_v2_1920x1280\ 2.png");
     background-repeat: no-repeat;
-    background-size: 80vw;
-    background-attachment:fixed;
+    background-size: 100vw;
 }
 .cabecalho, .user-interac, .pic{
     display: flex;
