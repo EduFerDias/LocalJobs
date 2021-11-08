38d1eec943b66ef7adaee48f61fa782e45a6830b
Luis <lupinha2014@gmail.com>
2021-11-08 20:11:41 -0300

    desempregados não

--- reactjs/src/pages/Pagina1/index.js
+++ reactjs/src/pages/Pagina1/index.js
@@ -47,7 +47,7 @@ export default function Pagina1 (){
                                     <img src="./assets/images/Cadastro e Login/p1-1693755 1.png" alt="" />
 
                                     <div className="button-text">
-                                        Cadastro para desempregados 
+                                        Cadastro para candidatos 
                                         <hr/>
                                     </div>
                                 </button>
