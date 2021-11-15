1f94cc580ff1ccfc0670b45ce446f1167f39fbf7
EduFerDias <Diasdu2011@outlook.com>
2021-11-15 19:42:34 -0300

    a lasco

--- api/src/controller/searchController.js
+++ api/src/controller/searchController.js
@@ -51,15 +51,29 @@ app.post('/buscarUsuario', async(req, resp) =>{
     }
 });
 app.post('/buscarEmpresa', async(req, resp) =>{
-    let {area, cidade} = req.body;
-    let e = ''         
-    if(area && cidade){
-         e = await db.infoc_atn_tb_empresa.findAll({where:{nm_ramo:area, ds_estado_cidade:{[Op.like]:`%${cidade}%`}}})        
-    }else if(!area && cidade){
-         e = await db.infoc_atn_tb_empresa.findAll({ where:{ds_estado_cidade:{[Op.like]:`%${cidade}%`}}})        
-    }else if(area && !cidade){
-         e = await db.infoc_atn_tb_empresa.findAll({where:{nm_ramo:area}})        
+    let {area, cargo, cidade} = req.body;         
+    if(area && cargo && cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({where:{nm_ramo:area, ds_cargo:cargo}, like:{ds_estado_cidade:cidade}})        
+        resp.send(r)
+    }else if(!area && cargo && !cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({where:{ds_cargo:cargo}})        
+        resp.send(r)
+    }else if(!area && !cargo && cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({like:{ds_estado_cidade:cidade}})        
+        resp.send(r)
+    }else if(area && !cargo && !cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({where:{nm_ramo:area}})        
+        resp.send(r)
+    }else if(!area && cargo && cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({where:{ds_cargo:cargo}, like:{ds_estado_cidade:cidade}})        
+        resp.send(r)
+    }else if(area && cargo && !cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({where:{nm_ramo:area, ds_cargo:cargo}})        
+        delete r.ds_senha;
+        resp.send(r)
+    }else if(area && !cargo && cidade){
+        let r = await db.infoc_atn_tb_empresa.findOne({where:{nm_ramo:area}, like:{ds_estado_cidade:cidade}})        
+        resp.send(r)
     }
-        resp.send(e)
 })
 export default app;
